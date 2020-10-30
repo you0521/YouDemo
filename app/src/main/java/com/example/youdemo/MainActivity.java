491d3664a43c8a8c5f1f09754a44675ac75fc90c
dell-PC\Administrator <jcy907394767@163.com>
2020-10-30 16:37:11 +0800

    按钮1：SuperButton

--- app/src/main/java/com/example/youdemo/MainActivity.java
+++ app/src/main/java/com/example/youdemo/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.youdemo;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.youdemo.Activity.ButtonOneActivity;
+import com.example.youdemo.Utils.IntentUtils;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    /**
+     * 按钮整的挺好看
+     */
+    private Button mBtnOne;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mBtnOne = (Button) findViewById(R.id.btn_one);
+        mBtnOne.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            default:
+                break;
+            case R.id.btn_one:
+                IntentUtils.startIntent(this, ButtonOneActivity.class);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/youdemo/Utils/IntentUtils.java
@@ -0,0 +1,57 @@
+package com.example.youdemo.Utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * author：  jcy
+ * data：2020/10/30
+ * time：16:31
+ * description：
+ */
+public class IntentUtils {
+
+    public static void startIntent(Activity activity,Class<?> cls){
+        Intent intent = new Intent(activity, cls);
+        activity.startActivity(intent);
+    }
+
+    /**
+     * 跳转
+     *
+     * @param mActivity
+     * @return
+     */
+    public static void startIntent(Activity mActivity, Class<?> cls, Bundle bundle) {
+        Intent mIntent = new Intent(mActivity, cls);
+        mIntent.putExtras(bundle);
+        mActivity.startActivity(mIntent);
+    }
+
+    /**
+     * 跳转
+     *
+     * @param mContext
+     * @return
+     */
+    public static void startIntent(Context mContext, Class<?> cls, Bundle bundle) {
+        Intent mIntent = new Intent(mContext, cls);
+        mIntent.putExtras(bundle);
+        mIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        mContext.startActivity(mIntent);
+    }
+
+    /**
+     * 跳转
+     *
+     * @param mActivity
+     * @return
+     */
+    public static void startIntentForResult(Activity mActivity, Class<?> cls, Bundle bundle, int requestCode) {
+        Intent mIntent = new Intent(mActivity, cls);
+        mIntent.putExtras(bundle);
+        mActivity.startActivityForResult(mIntent, requestCode);
+    }
+}
